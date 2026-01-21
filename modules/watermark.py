97e89fc8d2c34c105c860ebf86d96a65d7c81036
God-zx <dev440768@gmail.com>
2026-01-21 16:40:56 +0530

    Update watermark.py

--- modules/watermark.py
+++ modules/watermark.py
@@ -1,1 +1,21 @@
+import subprocess
+import os
 
+def add_moving_watermark(input_video, output_video):
+    cmd = [
+        "ffmpeg", "-y",
+        "-i", input_video,
+        "-vf",
+        "drawtext="
+        "fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:"
+        "text='@Sumit_Zx':"
+        "fontcolor=white@0.5:"
+        "fontsize=24:"
+        "shadowcolor=black:"
+        "shadowx=2:shadowy=2:"
+        "x=w-mod(t*100\\,w+tw):"
+        "y=h-40",
+        "-c:a", "copy",
+        output_video
+    ]
+    subprocess.run(cmd, check=True
